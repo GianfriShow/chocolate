21e029e8243d971db19c8efa775af9d71497f4ae
GianfriShow <131198417+GianfriShow@users.noreply.github.com>
2024-09-30 09:29:53 +0200

    Update chocolate.py
    changed first self.cocoa_percentage to self.type in produce() method

--- chocolate.py
+++ chocolate.py
@@ -7,4 +7,4 @@ class Chocolate:
         Chocolate.all_info[self.type] = {"cocoa_percentage": self.cocoa_percentage, "daily_max": self.daily_max, "available": self.daily_max, "sales": {'cakes':0,'bars':0,'hot_chocolates':0}}
         self.info = Chocolate.all_info[self.type]
     def produce(self):
-        print(f'The chocolate type is "{self.cocoa_percentage}" and the cocoa percentage is {self.cocoa_percentage}%')
+        print(f'The chocolate type is "{self.type}" and the cocoa percentage is {self.cocoa_percentage}%')
